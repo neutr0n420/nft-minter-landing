5d294e91034431da944f68ea72eb950bb86dd824
neutr0n420 <aryan.bramhane@ortigan.in>
2024-03-25 04:29:52 +0530

    make a nft miniting landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import WalletContextProvider from "@/components/WalletContextProvider";
 import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+const colors = {
+  background: '#1f1f1f',
+  accent: "#833BBE",
+  bodyText: "#ffffff",
+}
+
+const theme = extendTheme({ colors })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </ChakraProvider>
+  );
 }
